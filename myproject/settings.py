16c8f8d62826746630beba05bd54a5e2ea2d5445
Soomi Choe <breath@ewhain.net>
2024-08-12 21:51:53 +0900

    mysql 연결

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'qnartdb',#db명
         'USER': 'wildgaory', #db user 이름
         'PASSWORD': 'wildgaory', #db password
-        'HOST': '', #나중에 aws로 연결
+        'HOST': 'qnartdb.chkweamwumqs.ap-northeast-2.rds.amazonaws.com', #나중에 aws로 연결
         'PORT': '3306', #mysql 포트번호
     }
 }
