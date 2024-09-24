3458abadca6bb12303b271b0c5cffb5fd98a6283
MINYOUNG JOO <87117304+MinyoungJoo@users.noreply.github.com>
2024-09-24 15:43:36 +0900

    Update services.py
    artwork 수정

--- masterpiece/services.py
+++ masterpiece/services.py
@@ -1,6 +1,6 @@
 import os
 from openai import OpenAI
-from .models import Artwork, ChatSession, Message
+from .models import Artwork, ArtworkChatSession, ArtworkMessage
 from django.shortcuts import get_object_or_404
 from dotenv import load_dotenv
 import random
@@ -11,18 +11,18 @@ client = OpenAI()
 def get_random_artwork():
     return Artwork.objects.order_by('?').first()
 
-# 채팅 세션 생성 함수
-def create_chat_session(user, artwork):
-    return ChatSession.objects.create(user=user, artwork=artwork)
+# 명화 채팅 세션 생성 함수
+def create_artwork_chat_session(user, artwork):
+    return ArtworkChatSession.objects.create(user=user, artwork=artwork)
 
 # 각 세션에서 user, assistant 별 대화 content를 db에 기록
 def add_message_to_session(session, role, content):
-    Message.objects.create(session=session, role=role, content=content)
+    ArtworkMessage.objects.create(session=session, role=role, content=content)
 
-# gpt와 대화
-def chat_with_gpt(session, user_message):
+# 명화 기반 gpt와 대화
+def artwork_chat_with_gpt(session, user_message):
     # 현재 채팅 세션과 관련된 이전 메세지 가져오기. 맥락에 맞게 대화 이어나가기 위함.
-    previous_messages = Message.objects.filter(session=session).order_by('timestamp') # 현재 세션에 해당하는 메세지 가져와 메세지 시간순 정렬.
+    previous_messages = ArtworkMessage.objects.filter(session=session).order_by('timestamp') # 현재 세션에 해당하는 메세지 가져와 메세지 시간순 정렬.
     conversation_history = [
         {"role": msg.role, "content": msg.content} for msg in previous_messages
     ]
