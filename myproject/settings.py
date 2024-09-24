37ee97751fcdd866d31ec825cd140ad265989149
Soomi Choe <breath@ewhain.net>
2024-09-25 06:54:23 +0900

    서버 오류 수정 중

--- myproject/settings.py
+++ myproject/settings.py
@@ -178,3 +178,23 @@ print("STATIC_ROOT is set to:", STATIC_ROOT)
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#장고 로그파일 생성
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
