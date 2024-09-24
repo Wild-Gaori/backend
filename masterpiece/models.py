0cfbd0384b202f3773d93dbc765a5b340c320127
MINYOUNG JOO <87117304+MinyoungJoo@users.noreply.github.com>
2024-09-24 15:43:12 +0900

    Update models.py
    artwork 수정

--- masterpiece/models.py
+++ masterpiece/models.py
@@ -14,15 +14,15 @@ class Artwork(models.Model):
     def __str__(self):
         return self.title
 
-# 채팅 세션을 저장하는 모델
-class ChatSession(models.Model):
+# 채팅 세션을 저장하는 모델 (감상했던 명화 별로 관리)
+class ArtworkChatSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 사용자
     artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)  # 해당 채팅에서 사용된 명화
     created_at = models.DateTimeField(auto_now_add=True) # 채팅 세션 생성 시각
-
+    
 # 하나의 채팅 메시지 저장 모델 : 사용자 메시지나 GPT가 응답한 assistant 메시지를 저장
-class Message(models.Model):
-    session = models.ForeignKey(ChatSession, related_name='messages', on_delete=models.CASCADE) # 메세지 속한 채팅 세션 참조
+class ArtworkMessage(models.Model):
+    session = models.ForeignKey(ArtworkChatSession, related_name='messages', on_delete=models.CASCADE) # 메세지 속한 채팅 세션 참조
     role = models.CharField(max_length=10)  # 메세지 주체 : 'user' 또는 'assistant'
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
