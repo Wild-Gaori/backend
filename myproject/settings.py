c4088eedad3825187a3b3f2fe8b9433dcd298750
doodie <111527306+doodie104@users.noreply.github.com>
2024-10-01 13:53:26 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from dotenv import load_dotenv
 from openai import OpenAI 
-#from django.conf import settings
 
 # .env 파일 로드
 load_dotenv(verbose=True)
@@ -163,6 +162,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT 경로를 출력
 print("STATIC_ROOT is set to:", STATIC_ROOT)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 print("MEDIA_ROOT is set to:", MEDIA_ROOT)
 
 # Default primary key field type
