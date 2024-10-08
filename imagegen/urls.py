bd689046ea537505430aaf6648a6ddf81576a06d
doodie <111527306+doodie104@users.noreply.github.com>
2024-10-08 13:44:33 +0900

    Update urls.py

--- imagegen/urls.py
+++ imagegen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import generate_image, get_image_history
+from .views import generate_image, get_image_history,generate_image_method
 
 urlpatterns = [
     path('generate/', generate_image, name='generate_image'),
     path('history/', get_image_history, name='image_history'),
+    path('generate/method/', generate_image_method, name='generate_image_method'),
 ]
