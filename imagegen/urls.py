8e0cdab8cc3346d57c1902cea56269a620695017
doodie <111527306+doodie104@users.noreply.github.com>
2024-10-13 15:38:36 +0900

    Update urls.py

--- imagegen/urls.py
+++ imagegen/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('generate/method/', generate_image, name='generate_image'),
     path('history/', get_image_history, name='image_history'),
     path('generate/', generate_image_method, name='generate_image_method'),
+    path('generate/edit/', edit_image_with_dalle2, name='edit_image_with_dalle2'),
 ]
