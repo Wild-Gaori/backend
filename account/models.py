ede8fe01cc300397fd54f9bfca099af74eb680e8
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-12 06:28:25 +0900

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from docent.models import Docent
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -8,7 +9,7 @@ class UserProfile(models.Model):
     gender = models.CharField(max_length=10, blank=True, null=True)
     clothing = models.CharField(max_length=100, blank=True, null=True)
     hairstyle = models.CharField(max_length=100, blank=True, null=True)
-    selected_docent_id = models.IntegerField(default=1)
+    selected_docent = models.ForeignKey(Docent, on_delete=models.SET_DEFAULT, default=1, related_name='user_profiles')
 
     def __str__(self):
         return self.nickname
