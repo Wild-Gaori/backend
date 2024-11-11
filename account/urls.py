abad0a6eda5c73dbbc17189cb74664f0ab7807be
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-12 06:32:46 +0900

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import LoginView, SignupView, UpdateUserProfileView
+from .views import LoginView, SignupView, UpdateUserProfileView, GetUserProfileView
 from . import views
 
 
 
 urlpatterns = [
-    path('', LoginView.as_view(), name='login'), #로그인
+    path('login', LoginView.as_view(), name='login'), #로그인
     path('signup/', SignupView.as_view(), name='signup'), #회원가입
-    path('update-profile/', UpdateUserProfileView.as_view(), name='update_profile'),
+    path('update_profile/', UpdateUserProfileView.as_view(), name='update_profile'), #사용자 정보 생성, 수정
+    path('get-user-profile/', GetUserProfileView.as_view(), name='get_user_profile'), #기존 사용자 정보 반환
 ]  
