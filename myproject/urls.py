286aaf3fc8e72709201288ff84e10833ad59755d
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-12 06:34:09 +0900

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('account.urls')), # 로그인,회원가입
+    path('account/', include('account.urls')), # 로그인,회원가입
     path('masterpiece/', include('masterpiece.urls')),  # 명화 감상 앱
     path('imagegen/', include('imagegen.urls')), # 이미지 생성 앱 
 ] 
