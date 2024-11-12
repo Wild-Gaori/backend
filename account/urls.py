63b4aae4d08901165f9ee706cb69e43df284d010
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-12 13:21:27 +0900

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'), #로그인
     path('signup/', SignupView.as_view(), name='signup'), #회원가입
-    path('update_profile/', UpdateUserProfileView.as_view(), name='update_profile'), #사용자 정보 생성, 수정
+    path('update-profile/', UpdateUserProfileView.as_view(), name='update_profile'), #사용자 정보 생성, 수정
     path('get-user-profile/', GetUserProfileView.as_view(), name='get_user_profile'), #기존 사용자 정보 반환
 ]  
