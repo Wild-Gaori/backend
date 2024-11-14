35dced9e4d40a59de45c6230e1c148b9ac71531b
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-14 20:10:59 +0900

    Update urls.py

--- imagegen/urls.py
+++ imagegen/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import generate_image, get_image_history,generate_image_method,edit_image_with_dalle2
 
 urlpatterns = [
-    path('generate/method/', generate_image, name='generate_image'), # 안 씀
     path('history/', get_image_history, name='image_history'),
     path('generate/', generate_image_method, name='generate_image_method'), #경험,상상
     path('generate/edit/', edit_image_with_dalle2, name='edit_image_with_dalle2'),
