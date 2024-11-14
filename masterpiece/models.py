5777d20b5b3ace1f63af7acbc4f76bd0333a23bc
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-14 10:52:08 +0900

    Update models.py

--- masterpiece/models.py
+++ masterpiece/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from docent.models import Docent  # Docent 모델을 참조하기 위해 import
 
 # 작가 정보를 저장하는 모델
 class Artist(models.Model):
@@ -23,13 +24,14 @@ class Artwork(models.Model):
 
     def __str__(self):
         return self.title
-
+    
 # 채팅 세션을 저장하는 모델 (감상했던 명화 별로 관리)
 class ArtworkChatSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 사용자
     artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)  # 해당 채팅에서 사용된 명화
-    created_at = models.DateTimeField(auto_now_add=True) # 채팅 세션 생성 시각
+    created_at = models.DateTimeField(auto_now_add=True)  # 채팅 세션 생성 시각
     chat_history = models.JSONField(default=list)  # 대화 기록을 JSON 형식으로 저장
+    docent_at_chat = models.ForeignKey(Docent, on_delete=models.SET_DEFAULT, default=1)  # 대화 당시의 도슨트 정보
 
     def __str__(self):
         return f"Session with {self.user.username} on '{self.artwork.title}'"
