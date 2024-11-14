d8c2eb0e8293497eaa5560428d34e0925931d077
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-14 19:30:22 +0900

    Update urls.py

--- masterpiece/urls.py
+++ masterpiece/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import random_artwork_view, artwork_chat_view, artwork_chat_history_view
+from .views import random_artwork_view, artwork_chat_view, artwork_chat_history_view, copy_artwork_chat_session
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('random/', random_artwork_view, name='random_artwork'),  # 랜덤 명화 불러오기
     path('chat/', artwork_chat_view, name='chat'),  # GPT와 채팅
     path('chat/history/', artwork_chat_history_view, name='chat_history'),  # 채팅 기록 조회
+    path('chat/copy_session/', copy_artwork_chat_session, name='copy_artwork_chat_session'),  # 채팅 세션 복사
 ]
