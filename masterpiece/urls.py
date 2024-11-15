c5291a49f6b32e6efea7e1fb30488fa9045b1db9
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-15 21:40:41 +0900

    Update urls.py

--- masterpiece/urls.py
+++ masterpiece/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('chat/history/', artwork_chat_history_view, name='chat_history'),  # 채팅 기록 조회
     path('chat/copy_session/', copy_artwork_chat_session, name='copy_artwork_chat_session'),  # 채팅 세션 복사
 
-    path('completed/', completed_artworks_for_user, name='completed_artworks_for_user'),  # 전시 작품 감상 여부
+    path('gallery/completed/', completed_artworks_for_user, name='completed_artworks_for_user'),  # 전시 작품 감상 여부
     path('gallery/create_session/', create_artwork_chat_session_view, name='create_artwork_chat_session_view'),  # 미술관 전시 작품 채팅
     path('gallery/list/', get_gallery_artworks_list, name='get_gallery_artworks_list'),  # 미술관 작품 정보 불러오기
 ]
