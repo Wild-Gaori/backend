4edc47f799bba88e3fc5a07aa91a29bddf9d266d
doodie <111527306+doodie104@users.noreply.github.com>
2024-11-15 21:31:07 +0900

    Update urls.py

--- masterpiece/urls.py
+++ masterpiece/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import random_artwork_view, artwork_chat_view, artwork_chat_history_view, copy_artwork_chat_session
-from .views import completed_artworks_for_user, gallery_artwork_chat_view, get_gallery_artworks_list
+from .views import completed_artworks_for_user, create_artwork_chat_session_view, get_gallery_artworks_list
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,6 +11,6 @@ urlpatterns = [
     path('chat/copy_session/', copy_artwork_chat_session, name='copy_artwork_chat_session'),  # 채팅 세션 복사
 
     path('completed/', completed_artworks_for_user, name='completed_artworks_for_user'),  # 전시 작품 감상 여부
-    path('gallery/chat/', gallery_artwork_chat_view, name='gallery_artwork_chat'),  # 미술관 전시 작품 채팅
+    path('gallery/create_session/', create_artwork_chat_session_view, name='create_artwork_chat_session_view'),  # 미술관 전시 작품 채팅
     path('gallery/list/', get_gallery_artworks_list, name='get_gallery_artworks_list'),  # 미술관 작품 정보 불러오기
 ]
